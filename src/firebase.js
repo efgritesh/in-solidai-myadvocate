90db15296df1df4a971594593fa2aa092389d17c
efgritesh <efgritesh@gmail.com>
2026-04-14 17:37:53 +0530

    Restore same-origin Firebase auth domain

--- src/firebase.js
+++ src/firebase.js
@@ -8,15 +8,34 @@ import {
 import { getFunctions } from 'firebase/functions';
 import { getStorage } from 'firebase/storage';
 
+const isBrowser = typeof window !== 'undefined';
+const hostedAuthDomains = new Set([
+  'iadvocate.solidai.in',
+  'in-solidai-myadvocate.web.app',
+  'in-solidai-myadvocate.firebaseapp.com',
+]);
+
+const resolvedAuthDomain =
+  isBrowser && hostedAuthDomains.has(window.location.hostname)
+    ? window.location.hostname
+    : 'in-solidai-myadvocate.firebaseapp.com';
+
 const firebaseConfig = {
   apiKey: "AIzaSyDDrZjtxROSOOZYk6uUS76dCB_pl8Cc4cc",
-  authDomain: "in-solidai-myadvocate.firebaseapp.com",
+  authDomain: resolvedAuthDomain,
   projectId: "in-solidai-myadvocate",
   storageBucket: "in-solidai-myadvocate.firebasestorage.app",
   messagingSenderId: "86382052587",
   appId: "1:86382052587:web:66f2c37ee3609fb721aaae",
   measurementId: "G-D3XLWK923E"
 };
+
+if (isBrowser) {
+  console.log('[firebase] init', {
+    hostname: window.location.hostname,
+    authDomain: resolvedAuthDomain,
+  });
+}
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
